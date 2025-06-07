80a4fe100ce49b3d36e43f210b8c8b3870945f38
Dmitriy <mr.dima337@gmail.com>
2025-06-07 15:34:29 +0300

    feat: add pagination on main page

--- src/pages/CharacterPage/CharacterPage.jsx
+++ src/pages/CharacterPage/CharacterPage.jsx
@@ -4,23 +4,68 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import s from "./CharacterPage.module.css";
 import { Character } from "../../common/components/Character/Character";
+
 export const CharacterPage = () => {
   const [characters, setCharacters] = useState([]);
 
+  const [info, setInfo] = useState({
+    count: 0,
+    pages: 0,
+    next: null,
+    prev: null,
+  });
+
+  const fetchData = (url) => {
+    axios.get(url).then((res) => {
+      setCharacters(res.data.results);
+      setInfo(res.data.info);
+    });
+  };
+
   useEffect(() => {
-    axios
-      .get("https://rickandmortyapi.com/api/character")
-      .then((res) => setCharacters(res.data.results));
+    fetchData("https://rickandmortyapi.com/api/character");
   }, []);
 
+  const nextPageHandler = () => {
+    fetchData(info.next);
+  };
+
+  const previousPageHandler = () => {
+    fetchData(info.prev);
+  };
+
   return (
-    <div className="pageContainer">
-      <h1 className={"pageTitle"}>The Character Page</h1>
-      <div className={s.characters}>
-        {characters.map((character) => (
-          <Character character={character} key={character.id} />
-        ))}
-      </div>
+    <div className={"pageContainer"}>
+      <h1 className={"pageTitle"}>CharacterPage</h1>
+      {characters.length && (
+        <div>
+          {
+            <div className={s.characters}>
+              {characters.map((character) => (
+                <Character character={character} key={character.id} />
+              ))}
+            </div>
+          }
+
+          <div className={s.buttonContainer}>
+            <button
+              disabled={info.prev === null}
+              className="linkButton"
+              onClick={previousPageHandler}
+            >
+              Назад
+            </button>
+
+            <button
+              disabled={info.next === null}
+              className="linkButton"
+              onClick={nextPageHandler}
+            >
+              Вперед
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
